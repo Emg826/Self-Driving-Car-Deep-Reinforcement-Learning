3de4220591ffcfbc43d9cb190c4402508827e683
Eric Gabriel <egabriel19@wooster.edu>
2018-09-20 12:32:39 -0400

    create deep reinforcement system to control steering angle; established goals, wrote JSON entry (untested)

---
+++ drl_steering_control.py
@@ -0,0 +1,110 @@
+"""
+The purpose of this file is to create a deep reinforcement learning
+system to control the steering ONLY. The throttle will be applied such
+that the maintained speed is 25 mph.
+
+Therefore, the first goal is to get a deep reinforcement algorithm that can,
+at the very least, steer itself to avoid collisions. If the algorithm is
+able to accomplish this fairly quickly, lane keeping should be next.
+
+The second goal is to come up with a usable reward/penalty function. The
+intended function is (distance_since_last_infraction)^2 where infractions
+are things such as collisions and resets. Note: I'm not sure how to incorporate
+penalties (like hitting a pedestrian).
+
+The third and last goal (for this file at least) is to just figure out how to
+use multiple simulation images with a deep Q network. This means learning how
+use multiple images and how to actually implement the deep Q network.
+
+The hope is that this will help to create a reward function and deep network
+that could be taken outside the simulation and continue learning. Therefore,
+I have to figure out how to take advantage of the simulation (e.g., it
+tracks collisions) and how to keep it as realistic as possible.
+"""
+
+
+## COPY THIS INTO YOUR SETTING.JSON FILE FOR THE SIMULATION
+# (in your documents folder)
+
+"""
+  "Vehicles": {
+    "PhysXCar": {
+      "Cameras": {
+        "front": {
+          "CaptureSettings": [{
+              "ImageType": 0,
+              "Width": 512,
+              "Height" 256
+          }],
+          "Yaw": 0,
+        },
+        "right": {
+          "CaptureSettings": [{
+              "ImageType": 0,
+              "Width": 512,
+              "Height" 256
+          }],
+          "Yaw": -90,
+        },
+        "left": {
+          "CaptureSettings": [{
+              "ImageType": 0,
+              "Width": 512,
+              "Height" 256
+          }],
+          "Yaw": 90,
+        },
+        "rear": {
+          "CaptureSettings": [{
+              "ImageType": 0,
+              "Width": 512,
+              "Height" 256
+          }],
+          "Yaw": 180,
+        },
+
+      }
+    }
+  },
+"""
+
+
+import airsim
+import numpy as np
+import time
+
+
+
+# client init
+client = airsim.CarClient()
+client.confirmConnection()
+client.enableApiControl(True)
+
+# car controls struct init
+car_controls = airsim.CarControls()
+
+# collision info struct init --
+collision_info = client.simGetCollisionInfo()
+
+# simulation/client event loop
+time_step = 0
+
+while True:
+  print('Round {}'.format(time_step+1))
+  # request an image of the scene from the front facing camera
+  sim_img_response = client.simGetImages([airsim.ImageRequest(camera_name='left',
+                                                              image_type=airsim.ImageType.Scene,
+                                                              pixels_as_float=True,
+                                                              compress=False),
+                                          airsim.ImageRequest(camera_name='front',
+                                                              image_type=airsim.ImageType.Scene,
+                                                              pixels_as_float=True,
+                                                              compress=False),
+                                          airsim.ImageRequest(camera_name='right',
+                                                              image_type=airsim.ImageType.Scene,
+                                                              pixels_as_float=True,
+                                                              compress=False),
+                                          airsim.ImageRequest(camera_name='right',
+                                                              image_type=airsim.ImageType.Scene,
+                                                              pixels_as_float=True,
+                                                              compress=False)])
