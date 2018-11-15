6c5ea626a1a8e5dad369b5725369e5b8d7b9c4da
Emg826 <egabriel19@wooster.edu>
2018-11-15 18:34:38 -0500

    program to get each raw img from sim && outpu that raw img in color, in grayscale, and transformed via experiment. it's looking like disparitynormalized is pretty decent (see img  folder) w/ crazy transform function

---
+++ get_all_kinds_of_sim_imgs.py
@@ -0,0 +1,93 @@
+"""
+Output an image of each type from simulator just so to see what it looks like.
+
+ImageType values in airsim.ImageRequest(). These are mostly computer vision
+terms, so it helps to Google image search and see what such images look like.
+
+Scene = 0: raw image from camera?
+DepthPlanner = 1: all pts in plan[e] parallel to camera have same depth
+DepthPerspective = 2: depth from camera using projection ray that hits that pixel (point cloud)
+DepthVis = 3: closest pixels --> black && >100m away pixels --> white pixels
+DisparityNormalized = 4: ^ but normalized to [0, 1]?
+Segmentation = 5: give specific meshes (road, lines, sidewalk, etc.) specific vals in image;
+  allows for image to be "segmented" into meshes
+SurfaceNormals = 6: ?
+Infrared = 7: object ID 42 = (42,42,42); all else is grey scale
+"""
+
+import airsim
+import numpy as np
+import cv2
+import time
+import math
+
+# client init
+client = airsim.CarClient()
+client.confirmConnection()
+client.enableApiControl(True)
+client.reset()
+
+time.sleep(1) # wait for things to get setup 
+
+sim_img_responses = client.simGetImages([airsim.ImageRequest(camera_name='0',
+                                                                                      image_type=airsim.ImageType.Scene,
+                                                                                      pixels_as_float=False,
+                                                                                      compress=False),
+                                                          airsim.ImageRequest(camera_name='0',
+                                                                                      image_type=airsim.ImageType.DepthPlanner,
+                                                                                      pixels_as_float=False,
+                                                                                      compress=False),
+                                                          airsim.ImageRequest(camera_name='0',
+                                                                                      image_type=airsim.ImageType.DepthPerspective,
+                                                                                      pixels_as_float=False,
+                                                                                      compress=False),
+                                                          airsim.ImageRequest(camera_name='0',
+                                                                                      image_type=airsim.ImageType.DepthVis,
+                                                                                      pixels_as_float=False,
+                                                                                      compress=False),
+                                                          airsim.ImageRequest(camera_name='0',
+                                                                                      image_type=airsim.ImageType.DisparityNormalized,
+                                                                                      pixels_as_float=False,
+                                                                                      compress=False),
+                                                          airsim.ImageRequest(camera_name='0',
+                                                                                      image_type=airsim.ImageType.Segmentation,
+                                                                                      pixels_as_float=False,
+                                                                                      compress=False),
+                                                          airsim.ImageRequest(camera_name='0',
+                                                                                      image_type=airsim.ImageType.SurfaceNormals,
+                                                                                      pixels_as_float=False,
+                                                                                      compress=False),
+                                                          airsim.ImageRequest(camera_name='0',
+                                                                                      image_type=airsim.ImageType.Infrared,
+                                                                                      pixels_as_float=False,
+                                                                                      compress=False),])
+
+# raw imgs
+img_names = ['Scene', 'DepthPlanner', 'DepthPerspective', 'DepthVis', 'DisparityNormalized', 'Segmentation', 'SurfaceNormals', 'Infrared']
+for img_response_obj, img_name in zip(sim_img_responses, img_names):
+  height = img_response_obj.height
+  width = img_response_obj.width
+  img = np.fromstring(img_response_obj.image_data_uint8, dtype=np.uint8).reshape(height, width, 4)
+  cv2.imwrite('{}.jpg'.format(img_name), img)
+  cv2.imwrite('grayscale_{}.jpg'.format(img_name), img)
+
+
+# any experiments you'd want to do          
+img_names = ['Scene', 'DepthPlanner', 'DepthPerspective', 'DepthVis', 'DisparityNormalized']
+
+alter_pixel = lambda pixel_value: int( 255.0 * (1.0 / (1.0 + math.exp(-1.0 * (math.sqrt(2.71 * float(pixel_value)) - 4.5) ))) )
+for img_response_obj, img_name in zip(sim_img_responses, img_names):
+  height = img_response_obj.height
+  width = img_response_obj.width
+                   
+  np_arr_of_img_2d = cv2.cvtColor(np.fromstring(img_response_obj.image_data_uint8, dtype=np.uint8).reshape(height, width, 4),
+                                                                                          cv2.COLOR_BGR2GRAY)
+
+  for row_idx in range(0, np_arr_of_img_2d.shape[0]):
+    for col_idx in  range(0, np_arr_of_img_2d.shape[1]):
+      np_arr_of_img_2d[row_idx][col_idx] = alter_pixel(np_arr_of_img_2d[row_idx][col_idx])
+  
+  cv2.imwrite('experiment_{}.jpg'.format(img_name), np_arr_of_img_2d)
+
+
+
